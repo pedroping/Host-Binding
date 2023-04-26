e6b8b9eb29ddb95c33da9e063aed51c8ed156bf6
pedroping <84201034+pedroping@users.noreply.github.com>
2023-04-25 21:11:59 -0300

    1 file modified

--- src/rainbow.directive.ts
+++ src/rainbow.directive.ts
@@ -28,9 +28,9 @@ export class RainbowDirective {
 
   @HostListener('keydown') newColor() {
     const colorPick = Math.floor(Math.random() * this.possibleColors.length);
+    this.borderColor = this.possibleColors[colorPick];
     this.color = this.possibleColors[colorPick];
     this.outline = 'none !important';
-    this.borderColor = this.possibleColors[colorPick];
   }
 
   @HostListener('focus') onFocus() {
